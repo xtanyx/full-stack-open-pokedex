74c2fa485d105443788bc48bd8c49fc2131e54e0
xtanyx <xpawartanishqx@gmail.com>
2024-11-20 01:45:45 +0530

    ex 11.16 test-0.11

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req,res) => {
-  res.send('13 x')
+  res.send('14')
 })
 
 app.get('/health', (req, res) => {
